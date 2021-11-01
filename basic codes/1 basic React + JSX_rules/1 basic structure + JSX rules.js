ecbd770393777fda481d39cb419aa31d987506aa
NoumanShah042 <noumanrehman042@gmail.com>
2021-11-02 00:38:58 +0500

    adding files

---
+++ basic codes/1 basic React + JSX_rules/1 basic structure + JSX rules.js
@@ -0,0 +1,126 @@
+// creating basic component
+
+import React from "react"; //  React dependency
+import ReactDom from "react-dom";
+
+// this function will be a component
+// name should start with capital letter
+function Greeting() {
+	return <h4>This is Nouman , and this is my first component</h4>;
+	//  we are returning HTML , which is officially called JSX
+}
+
+// ReactDom.render(<Greeting>, document.getElementById('root'));     // error , Greeting tag not closed
+ReactDom.render(<Greeting />, document.getElementById("root")); // self closing tag
+ReactDom.render(<Greeting></Greeting>, document.getElementById("root")); // also works
+
+// **********************************
+
+import React from "react";
+import ReactDom from "react-dom";
+
+// JSX Rules
+
+// return single element
+// div / section / article or Fragment  ( use proper HTML semantics or use React.Fragment as a wrapper of JSX)
+// use camelCase for html attributes like onClick
+// use className instead of class
+// close every element  ( <img src="" alt="" />   or  <img src="" alt=""><\img>)
+// formatting ( return JSX in parenthesis so that there will be no formatting  error if any )
+
+function Greeting() {
+	return <h4>Hello world</h4>;
+}
+
+const Greeting = () => {
+	return React.createElement("h1", {}, "Hello World");
+	//     createElement ( html_element , props_object , html_element_content )
+};
+
+// ********
+
+function Greeting() {
+	// return multiple elements || return using ( ) and life will be easy
+	return (
+		<div>
+			<h1>Hello World</h1>
+		</div>
+	);
+}
+
+const Greeting = () => {
+	// return multiple elements with React.createElement
+	return React.createElement(
+		"div",
+		{},
+		React.createElement("h1", {}, "Hello World")
+	);
+};
+
+function Greeting() {
+	// different ways of wrapping multiple elements in single element
+	return (
+		// use parent div
+		<div>
+			<div>
+				<h1>Hello World</h1>
+				<ul>
+					<li>
+						<a href="#">click here</a>
+					</li>
+				</ul>
+			</div>
+			<div></div>
+		</div>
+	);
+
+	return (
+		<React.Fragment>
+			<div>
+				<h1>Hello World</h1>
+				<ul>
+					<li>
+						<a href="#">click here</a>
+					</li>
+				</ul>
+			</div>
+			<div></div>
+		</React.Fragment>
+	);
+
+	return (
+		<>
+			<div>
+				<h1>Hello World</h1>
+				<ul>
+					<li>
+						<a href="#">click here</a>
+					</li>
+				</ul>
+			</div>
+			<div></div>
+		</>
+	);
+}
+
+function Greeting() {
+	return (
+		<div onClick="" className="">
+			<div>
+				<h1>Hello World</h1>
+				<ul>
+					<li>
+						<a href="#">click here</a>
+					</li>
+				</ul>
+			</div>
+			<div></div>
+			<img src="" alt="" />
+			<input type="text" />
+		</div>
+	);
+}
+
+ReactDom.render(<Greeting />, document.getElementById("root")); // self closing tag
+
+// **********************************
---
+++ basic codes/6 List/1 simple list.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+// CSS
+import "./index.css";
+
+const names = ["Nouman", "Ali", "Ramzan"];
+const newNames = names.map((name) => {
+	return <h1>{name}</h1>;
+});
+
+function BookList() {
+	
+	return <section className="booklist">{newNames}</section>;
+}
+
+ReactDom.render(<BookList />, document.getElementById("root"));
